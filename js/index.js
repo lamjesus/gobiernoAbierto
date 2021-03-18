4e9a4fa08d13d9641553dbbd1f4fad8358440478
Luis alberto Sinisterra muñoz <lam_jesus@hotmail.com>
2021-03-18 11:05:27 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,15 @@
+
+
+function iniciar() {      
+  let imagen = document.getElementById('imagen').src= "img/Ventaja_1.svg";
+
+}
+
+function iniciar2() {      
+    let imagen = document.getElementById('imagen').src= "img/Ventaja_2.svg";
+  }
+
+function iniciar3() {      
+     document.getElementById('imagen').src= "img/Ventaja_3.svg";
+}
+
